9e1e9d0602a1c43829f41ce511773c9b7acc83e1
Artem <artem.shuvalov91@gmail.com>
2022-04-17 22:29:31 -0700

    Updated O(1) solution for function printing fractions

--- algo_hw1_4.py
+++ algo_hw1_4.py
@@ -1,26 +1,53 @@
-# Problem 4
+### Problem 4: Finds the fraction with O(1) including cyclical
 
-def string_to_fraction(s: str):
-    numerator = int(s[:s.find('/')])
-    denominator = int(s[s.find('/')+1:])
-    integer_part = str(numerator // denominator)
-    remainder = numerator % denominator
-    res = ""
-    storage = {}
-    while (remainder not in storage) and (remainder != 0):
-        # Save the result to dictionary
-        storage[remainder] = len(res)
-        # Multiply by 10 and get the remainder of remainder
-        remainder *= 10
-        residual = remainder // denominator
-        # Save to the value of residual
-        res += str(residual)
-        remainder = remainder % denominator
-    if remainder == 0:
-        return integer_part + '.' + res
-    else:
-        return integer_part + '.(' + res + ')'
+# Finds the start of the cycle
+def go_to_cycle(num, den):
+    i = num
+    j = num
+    while True:
+        i = (10 * i) % den
+        j = 10 * (10 * j % den) % den
+        if i == j:
+            return i
 
-print(string_to_fraction('1/3'))
-print(string_to_fraction('1/2'))
-print(string_to_fraction('1/8'))
+# Finds the length of the loop which iterates
+def loop_len(num, den):
+    rem = num
+    k = 0
+    while True:
+        rem = 10 * rem % den
+        k += 1
+        if rem == num:
+            return k
+
+def find_start(num, den, length):
+    i = num
+    j = num
+    for l in range(length):
+        j = 10 * j % den
+    k = 0
+    while True:
+        if i == j:
+            return k
+        i = 10 * i % den
+        j = 10 * j % den
+        k += 1
+
+def string_to_fraction_o1(num, den):
+    # num //= den
+    print(num // den, end='.')
+    num = num % den
+
+    num_new = go_to_cycle(num, den)
+    k = loop_len(num_new, den)
+    l_start = find_start(num, den, k)
+    for i in range(l_start):
+        print(10 * num // den, end='')
+        num = 10 * num % den
+    print('(', end='')
+    for i in range(k):
+        print(10 * num // den, end='')
+        num = 10 * num % den
+    print(')')
+
+string_to_fraction_o1(3, 5)
